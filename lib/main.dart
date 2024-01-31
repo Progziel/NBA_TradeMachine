67fe6bbe913a15b17f7d21f32c18efe3b4473607
muneebshahzad095 <muneebshahzad095@gmail.com>
2024-01-31 20:51:01 +0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home:  HomeDashboard());
+        home:  const HomeDashboard());
   }
 }
 class HomeDashboard extends StatelessWidget {
